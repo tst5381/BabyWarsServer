c6c80f6d4b45dfd4bb3254083f496ba361293a76
Babygogogo <101599838@qq.com>
2016-08-23 16:20:25 +0800

    [Modify] Sync with the client code.

--- babyWars/res/data/sceneWar/0000000000000003.lua
+++ babyWars/res/data/sceneWar/0000000000000003.lua
@@ -9,83 +9,10 @@ return {
     warField = {
         tileMap = {
             template = "FullTest",
-
-            grids = {
-                {
-                    GridIndexable = {
-                        gridIndex = {x = 17, y = 5},
-                    },
-                    objectID = 107,
-                },
-                {
-                    GridIndexable = {
-                        gridIndex = {x = 18, y = 5},
-                    },
-                    objectID = 107,
-                },
-                {
-                    GridIndexable = {
-                        gridIndex = {x = 19, y = 5},
-                    },
-                    objectID = 107,
-                },
-                {
-                    GridIndexable = {
-                        gridIndex = {x = 19, y = 4},
-                    },
-                    objectID = 107,
-                },
-                {
-                    GridIndexable = {
-                        gridIndex = {x = 17, y = 3},
-                    },
-                    objectID = 107,
-                },
-                {
-                    GridIndexable = {
-                        gridIndex = {x = 18, y = 3},
-                    },
-                    objectID = 107,
-                },
-                {
-                    GridIndexable = {
-                        gridIndex = {x = 19, y = 3},
-                    },
-                    objectID = 107,
-                },
-                {
-                    GridIndexable = {
-                        gridIndex = {x = 17, y = 2},
-                    },
-                    objectID = 107,
-                },
-                {
-                    GridIndexable = {
-                        gridIndex = {x = 17, y = 1},
-                    },
-                    objectID = 107,
-                },
-                {
-                    GridIndexable = {
-                        gridIndex = {x = 18, y = 1},
-                    },
-                    objectID = 107,
-                },
-                {
-                    GridIndexable = {
-                        gridIndex = {x = 19, y = 1},
-                    },
-                    objectID = 107,
-                },
-            },
         },
 
         unitMap = {
             template = "FullTest",
-
-            grids = {
-                -- There's a template map, so that the grids data is ignored even if it's not empty.
-            },
         },
     },
 
