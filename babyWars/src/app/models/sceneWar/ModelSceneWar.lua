59647575d7ad004bb364211abfc4e093235b479a
Babygogogo <101599838@qq.com>
2016-09-15 00:03:25 +0800

    [Modify] Refactor ModelUnit.

--- babyWars/src/app/models/sceneWar/ModelSceneWar.lua
+++ babyWars/src/app/models/sceneWar/ModelSceneWar.lua
@@ -197,7 +197,6 @@ local function initActorWarField(self, warFieldData)
     local actor = Actor.createWithModelAndViewName("sceneWar.ModelWarField", warFieldData)
     actor:getModel():setRootScriptEventDispatcher(self:getScriptEventDispatcher())
         :setModelPlayerManager(self:getModelPlayerManager())
-        :setModelWeatherManager(self:getModelWeatherManager())
 
     self.m_ActorWarField = actor
 end
