536c870e7260b740ec13c72424b444eb5153d859
Babygogogo <101599838@qq.com>
2016-10-14 15:40:07 +0800

    [Modify] Refactor ActionPublisher.

--- babyWars/src/app/utilities/ActionPublisher.lua
+++ babyWars/src/app/utilities/ActionPublisher.lua
@@ -5,13 +5,16 @@ local GameConstantFunctions = require("src.app.utilities.GameConstantFunctions")
 local GridIndexFunctions    = require("src.app.utilities.GridIndexFunctions")
 local SingletonGetters      = require("src.app.utilities.SingletonGetters")
 local TableFunctions        = require("src.app.utilities.TableFunctions")
+local VisibilityFunctions   = require("src.app.utilities.VisibilityFunctions")
 
-local getAdjacentGrids       = GridIndexFunctions.getAdjacentGrids
-local getModelPlayerManager  = SingletonGetters.getModelPlayerManager
-local getModelTileMap        = SingletonGetters.getModelTileMap
-local getModelTurnManager    = SingletonGetters.getModelTurnManager
-local getModelUnitMap        = SingletonGetters.getModelUnitMap
-local getUnitTypeWithTiledId = GameConstantFunctions.getUnitTypeWithTiledId
+local getAdjacentGrids          = GridIndexFunctions.getAdjacentGrids
+local getModelPlayerManager     = SingletonGetters.getModelPlayerManager
+local getModelTileMap           = SingletonGetters.getModelTileMap
+local getModelTurnManager       = SingletonGetters.getModelTurnManager
+local getModelUnitMap           = SingletonGetters.getModelUnitMap
+local getPlayerIndexWithTiledId = GameConstantFunctions.getPlayerIndexWithTiledId
+local getUnitTypeWithTiledId    = GameConstantFunctions.getUnitTypeWithTiledId
+local isUnitVisible             = VisibilityFunctions.isUnitOnMapVisibleToPlayerIndex
 
 --------------------------------------------------------------------------------
 -- The private functions that create actions for publish.
@@ -19,20 +22,11 @@ local getUnitTypeWithTiledId = GameConstantFunctions.getUnitTypeWithTiledId
 local creators = {}
 creators.createActionForProduceModelUnitOnTile = function(action, playerIndex)
     -- 生产了新部队后，生产者自己可能会发现隐藏的敌方部队，但这对生产者以外的玩家都是不可知的，因此广播的action必须删除这些数据。
-
-    if (getUnitTypeWithTiledId(action.tiledID) ~= "Submarine") then
-        -- 生产的部队不是潜艇，因此需要告诉其他玩家生产了什么东西。
+    local isDiving        = (getUnitTypeWithTiledId(action.tiledID) == "Submarine")
+    local unitPlayerIndex = getPlayerIndexWithTiledId(action.tiledID)
+    if (isUnitVisible(action.fileName, action.gridIndex, isDiving, unitPlayerIndex, playerIndex)) then
         return TableFunctions.clone(action, {"revealedUnits"})
     else
-        -- 生产的部队是潜艇，那么对于其他玩家而言，如果他正好有部队能够看到这个潜艇，那么就需要把潜艇数据发送给该敌方玩家。
-        local modelUnitMap = getModelUnitMap(action.fileName)
-        for _, adjacentGridIndex in ipairs(getAdjacentGrids(action.gridIndex, modelUnitMap:getMapSize())) do
-            local modelUnit = modelUnitMap:getModelUnit(adjacentGridIndex)
-            if ((modelUnit) and (modelUnit:getPlayerIndex() == playerIndex)) then
-                return TableFunctions.clone(action, {"revealedUnits"})
-            end
-        end
-
         -- 其他玩家没有部队能够看到潜艇，那么要隐藏潜艇数据。
         return {
             actionName = "ProduceModelUnitOnTile",
